e1611afed65b186a6ecab12b11d3e64f6769558a
wongnich <wongnich@gmail.com>
2017-10-05 17:20:42 -0700

    attempting to add for loop for looping over multiple eax addresses, look at README.md for source

--- assign1.c
+++ assign1.c
@@ -1,31 +1,48 @@
-#include <stdio.h>
-#include <string.h>
+	#include <stdio.h>
+	#include <string.h>
 
-int main(int argc, char **argv)
-{
+	int main(int argc, char **argv)
+	{
 
-	unsigned int eax;
-	unsigned int ebx;
-	unsigned int ecx;
-	unsigned int edx;
+		//unsigned int eax;
+		unsigned int ebx;
+		unsigned int ecx;
+		unsigned int edx;
 
-	char vendor[13];
-	
-	eax = 0x00;
-	
-	__asm__ __volatile__(
-		"cpuid;"
-		: "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
-		: "a"(eax)
-		);
+		char vendor[13];
+		
+		/*eax = 0x00;
+
+		__asm__ __volatile__(
+			"cpuid;"
+			: "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
+			: "a"(eax)
+			);
 
 
-	memcpy(vendor, (char*)&ebx, 4);
-	memcpy(vendor + 4, (char*)&edx, 4);
-	memcpy(vendor + 8, (char*)&ecx, 4);
-	vendor[12] = 0;
+		memcpy(vendor, (char*)&ebx, 4);
+		memcpy(vendor + 4, (char*)&edx, 4);
+		memcpy(vendor + 8, (char*)&ecx, 4);
+		vendor[12] = 0;
 
-	printf("%s\n", vendor);
-	
-	return 0;
-}
+		printf("%s\n", vendor);*/
+		unsigned int eax[16] = {[0 ... 15] = 0x20};
+		int i;
+		for(i=0; i<16; i++){
+			__asm__ __volatile__(
+			"cpuid;"
+			: "=a"(eax[i]), "=b"(ebx), "=c"(ecx), "=d"(edx)
+			: "a"(eax[i])
+			);
+
+
+		memcpy(vendor, (char*)&ebx, 4);
+		memcpy(vendor + 4, (char*)&edx, 4);
+		memcpy(vendor + 8, (char*)&ecx, 4);
+		vendor[12] = 0;
+
+		printf("%s\n", vendor);
+		}
+		
+		return 0;
+	}
