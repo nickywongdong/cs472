7b156412ecbdc6d6389905684dd5418f62f2939d
wongnich <wongnich@gmail.com>
2017-10-24 04:05:09 -0700

    removed need for concatenate function, keeping it cause it's cool

--- assign2/concat.c
+++ assign2/concat.c
@@ -1,5 +1,6 @@
 //neat integer concatenation function found on stack overflow
 //source: https://stackoverflow.com/questions/12700497/how-to-concatenate-two-integers-in-c
+//probably doesn't work on negatives
 
 unsigned concatenate(unsigned x, unsigned y) {
     unsigned pow = 10;
